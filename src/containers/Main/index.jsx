812cb67c555a1e5e52888ee9c622daf3e1843537
Victor Pereira <victor.cordeiro2@gmail.com>
2020-08-12 21:58:11 -0300

    Getting data from API + components

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,10 +1,27 @@
-import React, { memo } from 'react';
+import React, { memo, useState, useEffect, useCallback, useEffect } from 'react';
+import Api from '../../api';
+import { ContainerStyled } from './style'
 
 function Main() {
+
+    const [data, setData] = useState({});
+    const [country, setCountry] = useState('brazil');
+
+    const getCovidData = useCallback((country) => {
+        Api.getCountry(country)
+            .then(data => setData(data));
+    }, [])
+
+    useEffect(() => {
+        getCovidData(country)
+    }, [getCovidData], country);
+
     return (
-        <div>
-            Teste
-        </div>
+        <ContainerStyle>
+            <div className="mb-2">
+
+            </div>
+        </ContainerStyle>
     )
 }
 
