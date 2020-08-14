23b975f521a2b2ea9cd4eabd4fd44c326022f458
Victor Pereira <victor.cordeiro2@gmail.com>
2020-08-13 23:36:34 -0300

    Displaying and selecting countries

---
+++ src/containers/Main/components/Card.jsx
@@ -0,0 +1,21 @@
+import React, { memo } from 'react';
+import { Card as CardUI } from '../../../components';
+import {
+    LabelStyled,
+    ValueStyled,
+    CardContentStyled
+} from './style'
+
+function Card({ value, label, color}) {
+    return (
+        <CardUI>
+            <CardContentStyled>
+                <ValueStyled>{value}</ValueStyled>
+                <LabelStyled>{label}</LabelStyled>
+            </CardContentStyled>
+        </CardUI>
+    )
+}
+
+export default memo(Card);
+
--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,28 +1,43 @@
-import React, { memo, useState, useEffect, useCallback, useEffect } from 'react';
+import React, { memo, useState, useEffect, useCallback } from 'react';
 import Api from '../../api';
-import { ContainerStyled } from './style'
+import Board from './components/Board';
+import Panel from './components/Panel';
+import { ContainerStyled } from './style';
 
 function Main() {
-
-    const [data, setData] = useState({});
-    const [country, setCountry] = useState('brazil');
+    const [data, setData] = useState({})
+    const [country, setCountry] = useState('brazil')
+    const updateAt = new Date().toLocaleString()
 
     const getCovidData = useCallback((country) => {
         Api.getCountry(country)
-            .then(data => setData(data));
+            .then(data => setData(data))
     }, [])
 
     useEffect(() => {
         getCovidData(country)
-    }, [getCovidData], country);
+    }, [getCovidData, country])
+
+    const handleChange = ({ target }) => {
+        const country = target.value
+        setCountry(country)
+    }
 
     return (
-        <ContainerStyle>
+        <ContainerStyled>
             <div className="mb-2">
-
+                <Panel
+                    data={data}
+                    updateAt={updateAt}
+                    onChange={handleChange}
+                    country={country}
+                    getCovidData={getCovidData}
+                />
             </div>
-        </ContainerStyle>
+            <Board data={data} />
+
+        </ContainerStyled>
     )
 }
 
-export default memo(Main);
+export default memo(Main)
